efc43f574bc93423066ba151e1e7a86cc2cc2317
Ali Haqiqi <aliz@foresightanalytics.ca>
2022-12-18 23:46:28 +0330

    add lsp
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,33 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.setup()
